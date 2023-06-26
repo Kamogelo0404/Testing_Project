3c354ede7c752ed0670b3526045f76ca78e02eb8
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-06-26 15:10:15 +0200

    Added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KamogeloComponent } from './components/kamogelo/kamogelo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KamogeloComponent
   ],
   imports: [
     BrowserModule,
